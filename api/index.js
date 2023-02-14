ce6e426f10dffd0368f3f09c733cead3ca96575d
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-02-14 16:54:39 +0800

    feat: update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
+import prisma from './lib/prisma.js'
+
 export default async (req, res) => {
+
+  const users = await prisma.user.findMany()
+
   const data = {
-    msg: 'Hello world!'
+    msg: 'Hello world!',
+    data: users
   }
 
   res.status(200).json(data)
