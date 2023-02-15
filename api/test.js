09686db0f71e018279cb36beb759452986e2d6ae
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-02-15 19:08:09 +0800

    feat: update

--- api/test.js
+++ api/test.js
@@ -1,4 +1,13 @@
+import axios from 'axios'
+
 export default async (req, res) => {
+  axios({
+    method: 'get',
+    url: 'http://www.weather.com.cn/data/sk/101190408.html'
+  }).then(result => {
+    console.log(result)
+  })
+
   res.status(200).json({
     msg: 'request successful'
   })
