25f9b01605a28ae8ead2e183f6856c280be7e826
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-02-15 17:03:00 +0800

    feat: update

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -14,22 +14,6 @@ console.log(route.query)
 onMounted(() => {
   window.authCode = route.query.code
 
-  // axios({
-  //   headers: {
-  //     Accept: 'application/json',
-  //     'Content-Type': 'application/json'
-  //   },
-  //   method: 'POST',
-  //   url: 'https://github.com/login/oauth/access_token',
-  //   data: {
-  //     'client_id': '8936ce20162fada9e7ac',
-  //     'client_secret': 'e26ff315227a28793c406505ded51a73a85b3b0a',
-  //     code: route.query.code
-  //   }
-  // }).then(res => {
-  //   alert(JSON.stringify(res))
-  // })
-
   axios({
     url: '/api/user/auth',
     method: 'post',
